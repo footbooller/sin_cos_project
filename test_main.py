bdc84c110873e5457e6819cd5a8ac65f94430372
footbooller <mixaliov.nick@yandex.ru>
2024-11-06 13:09:04 +0300

    addition of 2 lines

--- test_main.py
+++ test_main.py
@@ -9,12 +9,14 @@ class TestTrigonometricFunctions(unittest.TestCase):
         self.assertAlmostEqual(calculate_sinus(np.pi / 2), 1)
         self.assertAlmostEqual(calculate_sinus(np.pi), 0)
         self.assertAlmostEqual(calculate_sinus(3 * np.pi / 2), -1)
+        self.assertAlmostEqual(calculate_sinus(np.pi / 4), np.sqrt(2) / 2)
 
     def test_calculate_cosine(self):
         self.assertAlmostEqual(calculate_cosinus(0), 1)
         self.assertAlmostEqual(calculate_cosinus(np.pi / 2), 0)
         self.assertAlmostEqual(calculate_cosinus(np.pi), -1)
         self.assertAlmostEqual(calculate_cosinus(3 * np.pi / 2), 0)
+        self.assertAlmostEqual(calculate_cosinus(np.pi / 4), np.sqrt(2) / 2)
 
 if __name__ == "__main__":
     unittest.main()
